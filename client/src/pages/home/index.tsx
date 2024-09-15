4bda11f9d303c4b5f7ab27a954cdc8f5685cf86c
QuyManh <quymanhtran453@gmail.com>
2024-09-15 22:58:04 +0700

    refactor(home): disable exhaustive-deps warning and remove unused code

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { AuthState } from "@/stores/auth";
 import { formatEther, parseEther } from "ethers";
